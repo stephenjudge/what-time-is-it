ba9b42b36cae6a0b0a7705f143bdf9ad087a5787
Kiran Busi <kiran.busi@gmail.com>
2012-04-19 21:55:27 +1000

    updated to show am/pm added expermiental tag for feature i'm adding

--- coffee/custom_time.coffee
+++ coffee/custom_time.coffee
@@ -3,6 +3,12 @@ monthNames = [ "January", "February", "March", "April", "May", "June", "July", "
 dayNames = [ "Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday" ]
 offset = -4 # Default to tampa, fl
 
+check_am_pm = (d) ->
+  if d > 12
+    return "pm"
+  else
+    return "am"
+
 update_time_periodically = ->
   citya_date = new Date() # Always defaults to current time, current city
   ta_secs = citya_date.getSeconds()
@@ -11,25 +17,34 @@ update_time_periodically = ->
   ta_mins = citya_date.getMinutes()
   $("#min").html padNumber(ta_mins)
   ta_hrs = citya_date.getHours()
+  am_pm_a = check_am_pm(ta_hrs)
+  #console.log "Am or pm? " + am_pm
+  $("#am_pm_ind_a").html(am_pm_a)
   ta_hrs = make_hrs_human_readable(ta_hrs)
   $("#hours").html padNumber(ta_hrs)
 
-  cityb_date = calcTime(offset)      # Get it fresh as possible
+  cityb_date = calcTime(offset, new Date() )      # Get it fresh as possible
   tb_secs = cityb_date.getSeconds()
   #console.log "Target city secs: " + tb_secs
   $("#sec1").html padNumber(tb_secs)
   tb_min = cityb_date.getMinutes()
   $("#min1").html padNumber(tb_min)
   tb_hrs = cityb_date.getHours()
+  am_pm_b = check_am_pm(tb_hrs)
+  #console.log "Am or pm? " + am_pm_b
+  $("#am_pm_ind_b").html(am_pm_b)
   tb_hrs = make_hrs_human_readable(tb_hrs)
   $("#hours1").html padNumber(tb_hrs)
+
 padNumber = (n) ->
   (if n < 10 then "0" else "") + n
+
 make_hrs_human_readable = (h) ->
   h = parseInt(h) - parseInt(12)  if h > 12
   h
-calcTime = (offset) ->
-  d = new Date()
+
+calcTime = (offset, aDate) ->
+  d = aDate
   utc = d.getTime() + (d.getTimezoneOffset() * 60000)
   nd = new Date(utc + (3600000 * offset))
   nd
@@ -49,18 +64,18 @@ update_time_for_city_b = (location) ->
 # Update right side of screen with correct time of city input
 processAndUpdateScreen = (json) ->
   offset = json.data.time_zone[0].utcOffset
-  target_city_time = calcTime(offset)
+  target_city_time = calcTime(offset, new Date() )
   console.log "target city: " + target_city_time
   update_target_city_name($("#f_elem_city").val())
   $("#f_elem_city").val ""                        #Reset target city input box
 
 
 # JSON URL
-city_autocomplete_url = "http://gd.geobytes.com/AutoCompleteCity?callback=?"
+city_autocomplete_url = "http://gd.geobytes.com/AutoCompleteCity?callback=?&q="
 jQuery ->
   jQuery("#f_elem_city").autocomplete
     source: (request, response) ->
-      jQuery.getJSON city_autocomplete_url+"&q=" + request.term, (data) ->
+      jQuery.getJSON city_autocomplete_url+ request.term, (data) ->
         response data
 
     minLength: 3
@@ -84,15 +99,28 @@ update_target_city_name = (target_city_name) ->
   $("#rt_cap").html target_city_name
   $("#target_city").html target_city_name
 
-time_there_at = ->
-  console.log "Time here when it's " + $("#time_a").val() + " in city " + $("#target_city").html()
+process_time_input = (t) ->
+  s = t.split "pm"
+  s = s[0]
+  console.log "Split str: " + s
+  s
 
+time_there_at = ->
+  t = $("#time_a").val()
+  console.log "Time here when it's " + t + " in city " + $("#target_city").html()
+  process_time_input("2pm")
+  s = process_time_input(t)
+  d = new Date()
+  d.setHours(19)
+  console.log "Time to calculate: " + d
+  d_a = calcTime(new Date().getTimezoneOffset(), d)
+  console.log "Time here should be: " + d_a
 
 # When page loads...
 $ ->
 
   time_now = new Date()
-  citybDate = calcTime(offset)
+  citybDate = calcTime(offset, new Date() )
 
   # Update name of current city on left side
   $("#lft_cap").html geoplugin_city() + ", " + geoplugin_region() + ", " + geoplugin_countryCode()
--- js/custom_time.js
+++ js/custom_time.js
@@ -1,5 +1,5 @@
 (function() {
-  var calcTime, city_autocomplete_url, dayNames, make_hrs_human_readable, monthNames, offset, padNumber, processAndUpdateScreen, time_there_at, update_target_city_name, update_time_for_city_b, update_time_periodically;
+  var calcTime, check_am_pm, city_autocomplete_url, dayNames, make_hrs_human_readable, monthNames, offset, padNumber, processAndUpdateScreen, process_time_input, time_there_at, update_target_city_name, update_time_for_city_b, update_time_periodically;
 
   monthNames = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
 
@@ -7,22 +7,34 @@
 
   offset = -4;
 
+  check_am_pm = function(d) {
+    if (d > 12) {
+      return "pm";
+    } else {
+      return "am";
+    }
+  };
+
   update_time_periodically = function() {
-    var citya_date, cityb_date, ta_hrs, ta_mins, ta_secs, tb_hrs, tb_min, tb_secs;
+    var am_pm_a, am_pm_b, citya_date, cityb_date, ta_hrs, ta_mins, ta_secs, tb_hrs, tb_min, tb_secs;
     citya_date = new Date();
     ta_secs = citya_date.getSeconds();
     $("#sec").html(padNumber(ta_secs));
     ta_mins = citya_date.getMinutes();
     $("#min").html(padNumber(ta_mins));
     ta_hrs = citya_date.getHours();
+    am_pm_a = check_am_pm(ta_hrs);
+    $("#am_pm_ind_a").html(am_pm_a);
     ta_hrs = make_hrs_human_readable(ta_hrs);
     $("#hours").html(padNumber(ta_hrs));
-    cityb_date = calcTime(offset);
+    cityb_date = calcTime(offset, new Date());
     tb_secs = cityb_date.getSeconds();
     $("#sec1").html(padNumber(tb_secs));
     tb_min = cityb_date.getMinutes();
     $("#min1").html(padNumber(tb_min));
     tb_hrs = cityb_date.getHours();
+    am_pm_b = check_am_pm(tb_hrs);
+    $("#am_pm_ind_b").html(am_pm_b);
     tb_hrs = make_hrs_human_readable(tb_hrs);
     return $("#hours1").html(padNumber(tb_hrs));
   };
@@ -36,9 +48,9 @@
     return h;
   };
 
-  calcTime = function(offset) {
+  calcTime = function(offset, aDate) {
     var d, nd, utc;
-    d = new Date();
+    d = aDate;
     utc = d.getTime() + (d.getTimezoneOffset() * 60000);
     nd = new Date(utc + (3600000 * offset));
     return nd;
@@ -63,18 +75,18 @@
   processAndUpdateScreen = function(json) {
     var target_city_time;
     offset = json.data.time_zone[0].utcOffset;
-    target_city_time = calcTime(offset);
+    target_city_time = calcTime(offset, new Date());
     console.log("target city: " + target_city_time);
     update_target_city_name($("#f_elem_city").val());
     return $("#f_elem_city").val("");
   };
 
-  city_autocomplete_url = "http://gd.geobytes.com/AutoCompleteCity?callback=?";
+  city_autocomplete_url = "http://gd.geobytes.com/AutoCompleteCity?callback=?&q=";
 
   jQuery(function() {
     jQuery("#f_elem_city").autocomplete({
       source: function(request, response) {
-        return jQuery.getJSON(city_autocomplete_url + "&q=" + request.term, function(data) {
+        return jQuery.getJSON(city_autocomplete_url + request.term, function(data) {
           return response(data);
         });
       },
@@ -102,14 +114,31 @@
     return $("#target_city").html(target_city_name);
   };
 
+  process_time_input = function(t) {
+    var s;
+    s = t.split("pm");
+    s = s[0];
+    console.log("Split str: " + s);
+    return s;
+  };
+
   time_there_at = function() {
-    return console.log("Time here when it's " + $("#time_a").val() + " in city " + $("#target_city").html());
+    var d, d_a, s, t;
+    t = $("#time_a").val();
+    console.log("Time here when it's " + t + " in city " + $("#target_city").html());
+    process_time_input("2pm");
+    s = process_time_input(t);
+    d = new Date();
+    d.setHours(19);
+    console.log("Time to calculate: " + d);
+    d_a = calcTime(new Date().getTimezoneOffset(), d);
+    return console.log("Time here should be: " + d_a);
   };
 
   $(function() {
     var citybDate, time_now;
     time_now = new Date();
-    citybDate = calcTime(offset);
+    citybDate = calcTime(offset, new Date());
     $("#lft_cap").html(geoplugin_city() + ", " + geoplugin_region() + ", " + geoplugin_countryCode());
     $("#Date").html(dayNames[citybDate.getDay()] + " " + citybDate.getDate() + " " + monthNames[citybDate.getMonth()] + " " + citybDate.getFullYear());
     $("#Date1").html(dayNames[time_now.getDay()] + " " + time_now.getDate() + " " + monthNames[time_now.getMonth()] + " " + time_now.getFullYear());
